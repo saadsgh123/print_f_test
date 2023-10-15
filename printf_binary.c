6da805ede4f41515a10bc245f545d4007d17a4c1
saadsgh123 <sdsghouri@gmail.com>
2023-10-15 13:28:49 +0100

    diedjeidj

--- printf_binary.c
+++ printf_binary.c
@@ -1,15 +1,27 @@
 #include "main.h"
+/**
+ * printf_binary - prints a binary number.
+ * @d: arguments.
+ * Return: 1.
+ */
 void decimalTobinary(unsigned int d)
 {
-	int quotient[32], i = 0;
-    if (d == 0) {
-        _putchar('0');
-        return;
-    }
-    while (d > 0) {
-        quotient[i] = d % 2;
-        d = d / 2;
+	if (d == 0)
+	{
+        	_putchar('0');
+        	return;
+    	}
+	int quotient[32];
+	int i = 0;
+	while(d > 0)
+	{
+		quotient[i] = d % 2;
+		d = d / 2;
 
-        i++;
-    }
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		_putchar( quotient[j] + '0');
+	}
 }
--- printf_string.c
+++ printf_string.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
-int  print_string(char *str)
+/**
+ * print_string - Returns the lenght of a string.
+ * @str: Type char pointer
+ * Return: c.
+ */
+int print_string(char *str)
 {
-        int counte = 0, i;
-for (i = 0;str[i] != '\0';i++ )
-        {
-                _putchar(str[i]);
-                counte++;
-        }
-return (0);
+	int counte = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		counte++;
+	}
+	return (0);
 }
