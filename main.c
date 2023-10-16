7ead37cba2f6879a2dc560511c3e4ca6ccd63cef
saadsgh123 <sdsghouri@gmail.com>
2023-10-16 13:49:11 +0100

    dedejidje

--- main.c
+++ main.c
@@ -1,41 +1,17 @@
 #include "main.h"
 int main() {
-    int num1 = 42;
-    int num2 = -123;
-    int num3 = 0;
-    int num4 = 2147483647;
-    int num5 = -2147483648;
-    int num6 = 7;
-    int num7 = 42;
+    int len;
+    len = _printf("%s%c%c%c%s%%%s%c","Loading",'.','.','.'," 99","Please wait", '\n');
+    printf("%d",len);
+    printf("%s%c%c%c%s%%%s%c","Loading",'.','.','.'," 99","Please wait", '\n');
 
-    // Test with your custom _printf
-    printf("Custom _printf:\n");
-    _printf("Positive Integer: %d\n", num1);
-    _printf("Negative Integer: %d\n", num2);
-    _printf("Zero: %d\n", num3);
-    _printf("Large Integer: %d\n", num4);
-    _printf("Smallest Integer: %d\n", num5);
-    _printf("Mix of Format Specifiers: %d %i\n", num6, num7);
-    _printf("Leading and Trailing Text: The answer is: %d, correct?\n", num1);
-    _printf("Multiple Format Specifiers: %d %i %d\n", num1, num2, num3);
-    _printf("Format Specifier with Width and Precision: %05d, %2i\n", num1, num2);
-    _printf("Mix of Different Data Types: %d %i %d\n", num1, (short)num2, num3);
-    _printf("No Integer Argument: %d\n");
-    _printf("Mix with text: Number: %d\n", num1);
 
-    // Test with standard printf for comparison
-    printf("\nStandard printf:\n");
-    printf("Positive Integer: %d\n", num1);
-    printf("Negative Integer: %d\n", num2);
-    printf("Zero: %d\n", num3);
-    printf("Large Integer: %d\n", num4);
-    printf("Smallest Integer: %d\n", num5);
-    printf("Mix of Format Specifiers: %d %d\n", num6, num7);
-    printf("Leading and Trailing Text: The answer is: %d, correct?\n", num1);
-    printf("Multiple Format Specifiers: %d %d %d\n", num1, num2, num3);
-    printf("Format Specifier with Width and Precision: %05d, %2d\n", num1, num2);
-    printf("Mix of Different Data Types: %d %d %d\n", num1, (short)num2, num3);
-    printf("Mix with text: Number: %d\n", num1);
+
+    _printf("%%\n");
+    printf("%%\n");
+
+    _printf("%c%cth%s%s a%cg%s: Y%sou %s no%ching%s Snow. %c", 'W', 'i', "some", "more",'r',"s","","know",'t',", jon",'\n');
+    printf("%c%cth%s%s a%cg%s: Y%sou %s no%ching%s Snow. %c", 'W', 'i', "some", "more",'r',"s","","know",'t',", jon",'\n');
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include "stdarg.h"
